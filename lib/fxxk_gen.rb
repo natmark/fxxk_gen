0d0be77e026febaf25571d1d729a27e6d9b7722c
natmark <natmark0918@gmail.com>
2018-05-02 00:07:45 +0900

    :ok: Change lib/fxxk_gen.rb

--- lib/fxxk_gen.rb
+++ lib/fxxk_gen.rb
@@ -1,5 +1,27 @@
 require "fxxk_gen/version"
 
-module FxxkGen
-  # Your code goes here...
+class Brainfuck
+    def initialize
+    end
+
+    def self.bf_mapping
+        @bf_operations ||= {nxt: '>', prv: '<', inc: '+', dec: '-', put: '.',  get: ',', opn: '[', cls: ']' }
+    end
+
+    def self.default_mapping
+        @default_mapping ||= bf_mapping.clone
+    end
+
+    def generate(src)
+        # self.class.bf_mapping[:nxt] // >
+        # self.class.bf_mapping.invert[">"] //:nxt
+    end
+
+    class << self
+      Brainfuck.default_mapping.keys.each do |op|
+        define_method(op) do |val|
+          default_mapping[op] = val
+        end
+      end
+    end
 end
