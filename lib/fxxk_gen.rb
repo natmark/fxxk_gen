6aa0ef67dc45c9c5e492128780bd72ace0639bbf
natmark <natmark0918@gmail.com>
2018-05-02 15:48:20 +0900

    :+1: Add generate function

--- lib/fxxk_gen.rb
+++ lib/fxxk_gen.rb
@@ -1,6 +1,8 @@
 require "fxxk_gen/version"
 
 class Brainfuck
+    BLOCKS = 10
+
     def initialize
     end
 
@@ -12,16 +14,60 @@ class Brainfuck
         @default_mapping ||= bf_mapping.clone
     end
 
-    def generate(src)
-        # self.class.bf_mapping[:nxt] // >
-        # self.class.bf_mapping.invert[">"] //:nxt
+    def generate(text)
+        result = ""
+        prev_char_code = 0
+
+        text.chars.each_with_index do |c, i|
+            char_code = c.ord
+            gap = char_code - prev_char_code
+
+            division = { quotient: gap.abs / BLOCKS, reminder: gap.abs % BLOCKS }
+
+            if division[:quotient] != 0 && i > 0 then
+                result += self.class.default_mapping[:prv]
+            end
+
+            mark = gap < 0 ? self.class.default_mapping[:dec] : self.class.default_mapping[:inc]
+            result += optimize(division, mark)
+
+            prev_char_code  = char_code
+        end
+
+        return result
+    end
+
+    def optimize(division, mark)
+        result = ""
+
+        if division[:quotient] == 0 then
+            division[:reminder].times do
+                result += mark
+            end
+            result += self.class.default_mapping[:put]
+        else
+            BLOCKS.times do
+                result += self.class.default_mapping[:inc]
+            end
+            result += (self.class.default_mapping[:opn] + self.class.default_mapping[:nxt])
+            division[:quotient].times do
+                result += mark
+            end
+            result += (self.class.default_mapping[:prv] + self.class.default_mapping[:dec] + self.class.default_mapping[:cls] + self.class.default_mapping[:nxt])
+            division[:reminder].times do
+                result += mark
+            end
+            result += self.class.default_mapping[:put]
+        end
+
+        return result
     end
 
     class << self
-      Brainfuck.default_mapping.keys.each do |op|
-        define_method(op) do |val|
-          default_mapping[op] = val
+        Brainfuck.default_mapping.keys.each do |op|
+            define_method(op) do |val|
+                default_mapping[op] = val
+            end
         end
-      end
     end
 end
--- spec/fxxk_gen_spec.rb
+++ spec/fxxk_gen_spec.rb
@@ -1,9 +1,21 @@
-RSpec.describe FxxkGen do
-  it "has a version number" do
-    expect(FxxkGen::VERSION).not_to be nil
-  end
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+class Ook < Brainfuck
+    nxt 'Ook. Ook?'
+    prv 'Ook? Ook.'
+    inc 'Ook. Ook.'
+    dec 'Ook! Ook!'
+    put 'Ook! Ook.'
+    get 'Ook. Ook!'
+    opn 'Ook! Ook?'
+    cls 'Ook? Ook!'
+end
+
+RSpec.describe FxxkGen do
+    context 'customized class' do
+        subject { Ook.new }
+        it "generate('Hello World') should be 'Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook! Ook?O...Ook! Ook!Ook! Ook.Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook.'" do
+            expect(subject.generate('Hello World')).to eq("Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook. Ook.Ook! Ook?O...Ook! Ook!Ook! Ook.Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook!Ook! Ook.")
+        end
+    end
 end
